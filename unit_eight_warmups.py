cb213a1876183293b9632545e8442fda5117cd6b
CJ <cj@iMac7.local>
2018-11-13 09:14:24 -0500

    Clean up GUI

--- unit_eight_warmups.py
+++ unit_eight_warmups.py
@@ -10,6 +10,9 @@ def f_to_c():
 root = tkinter.Tk()
 f_value = tkinter.StringVar()
 c_value = tkinter.StringVar()
+
+f_value.set("0")
+
 root.title("Temperature Converter")
 F_Label = tkinter.Label(root, text="degrees F: ")
 F_Label.grid(row=1, column=1)
@@ -21,9 +24,9 @@ C_Label = tkinter.Label(root, text="degrees C:")
 C_Label.grid(row=2, column=1)
 
 C_Temp_Label = tkinter.Label(root, textvariable=c_value)
-C_Temp_Label.grid(row=2, column=2)
+C_Temp_Label.grid(row=2, column=2, sticky="W")
 
 convert_button = tkinter.Button(root, text="Convert", command=f_to_c)
-convert_button.grid(row=3, column=1)
+convert_button.grid(row=3, column=1, columnspan=2)
 
 root.mainloop()
